34d6d13d8f8c51a025060874bf91c130c5c27b5f
tunmie <victoroluwasomidotun@gmail.com>
2022-09-08 08:42:36 +0100

    about route done

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello_world():  # put application's code here
 	return render_template("home.html")
 
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
