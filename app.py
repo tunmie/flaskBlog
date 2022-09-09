f2906b97746479e85510d03ccf3197b685079d30
tunmie <victoroluwasomidotun@gmail.com>
2022-09-09 04:43:00 +0100

    register route done

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect
+from flask_mysqldb import MySQL
+from passlib.handlers.sha2_crypt import sha256_crypt
 from wtforms import Form, StringField, validators, PasswordField
 
 from article_data import Articles
@@ -6,6 +8,15 @@ from article_data import Articles
 app = Flask(__name__)
 
 Articles = Articles()
+
+# Config MySQL DB
+app.config['MYSQL_HOST'] = 'localhost'
+app.config["MYSQL_USER"] = 'root'
+app.config["MYSQL_PASSWORD"] = 'Tomodachi@2001'
+app.config["MYSQL_DB"] = 'flask_blog_app'
+app.config["MYSQL_CURSORCLASS"] = 'DictCursor'
+
+mysql = MySQL(app)
 
 
 class RegisterForm(Form):
@@ -19,10 +30,29 @@ class RegisterForm(Form):
 	confirm = PasswordField('Confirm Password')
 
 
-@app.route('/register', methods =['GET', "POST"])
+@app.route('/register', methods=['GET', "POST"])
 def register():
 	form = RegisterForm(request.form)
+	if request.method == "POST" and form.validate():
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.hash(str(form.password.data))
+
+		# Creates cursor
+		cur = mysql.connection.cursor()
+
+		cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+		mysql.connection.commit()
+
+		cur.close()
+
+		flash('You are now registered and may login. Welcome to BlogIt!', 'success')
+
+		redirect(url_for('articles'))
+
 	return render_template('register.html', form=form)
+
 
 @app.route('/')
 def home():  # put application's code here
@@ -46,3 +76,5 @@ def display_article(id):
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+app.secret_key = 'Secret145'
